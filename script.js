c5a28653466e4a3eadbdca5bf3411aa9da2dbe4a
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-02-27 19:22:58 +0530

    functionality added

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ function getAPIData(){
     request.send()
 
     request.addEventListener("load",()=>{
-        console.log(request.responseText);
-        
-    })
+        let data =  JSON.parse(request.responseText)[0]
+        document.getElementById("name").innerHTML = data.name.official
+        document.getElementById("capital").innerHTML = data.capital
+        document.getElementById("flags").src = data.flags.png
+        document.getElementById("area").innerHTML = data.area
+        document.getElementById("population").innerHTML = data.population
+        document.getElementById("region").innerHTML = data.region
+        document.getElementById("subregion").innerHTML = data.subregion
+        document.getElementById("continents").innerHTML = data.continents
+        document.getElementById("borders").innerHTML = data.borders
+        document.getElementById("timezones").innerHTML = data.timezones
+        document.getElementById("landlocked").innerHTML = data.landlocked
+        document.getElementById("unMember").innerHTML = data.unMember
+        document.getElementById("independent").innerHTML = data.independent
+        document.getElementById("maps").href = data.maps.googleMaps
+ })
 }
+
+getAPIData()
